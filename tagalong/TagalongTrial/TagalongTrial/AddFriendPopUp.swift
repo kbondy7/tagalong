455f22493a3df30689299daa9de02a6bae620992
Kieran Bondy <k.bondmna7@gmail.com>
2019-06-02 19:30:19 -0500

    updated pages

---
+++ tagalong/TagalongTrial/TagalongTrial/AddFriendPopUp.swift
@@ -0,0 +1,30 @@
+//
+//  AddFriendPopUp.swift
+//  TagalongTrial
+//
+//  Created by Kieran Bondy on 5/30/19.
+//  Copyright © 2019 Kieran Bondy. All rights reserved.
+//
+
+import UIKit
+
+class AddFriendPopUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
